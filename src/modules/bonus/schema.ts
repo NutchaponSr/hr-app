63a72ab5220a1a1ccb95e8fee9bdd7d60ee91436
NutchaponSr <sr.nutchapon@gmail.com>
2025-11-06 10:17:37 +0700

    feat: format number to string target120

--- src/modules/bonus/schema.ts
+++ src/modules/bonus/schema.ts
@@ -25,9 +25,12 @@ export const kpiBonusCreateSchema = z.object({
   strategy: z.string().min(1, "Required").trim(),
   method: z.string().min(1, "Required").trim(),
   type: currentYear === 2025 ? z.string().nullable() : z.string("Required"),
-  target120: currentYear === 2025 ? z.string().nullable() : z.coerce.string().min(1, "Required").trim(),
-  target100: z.coerce.string().trim(),
-  target90: z.coerce.string().trim(),
+  target120:
+    currentYear === 2025
+      ? z.coerce.string().nullable()
+      : z.coerce.string().trim().nullable(),
+  target100: z.coerce.string().trim().nullable(),
+  target90: z.coerce.string().trim().nullable(),
   target80: z.coerce.string().min(1, "Required").trim(),
   target70: z.coerce.string().min(1, "Required").trim(),
 });
